8d375f7bdab0e26fb84ccd1bb7b4d410b2a9daf9
KyungjinJPark <kjparkcontact@gmail.com>
2020-12-28 01:54:10 -0500

    Recent News previews now link to independent article sites

--- src/components/RecentNews.js
+++ src/components/RecentNews.js
@@ -1,25 +1,49 @@
+import {
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 import { Query } from 'react-apollo';
 import { Card } from 'react-bootstrap';
 
 import { NEWS_QUERY } from './all-products/queries';
 
 const RecentNews = () => {
+  let match = useRouteMatch();
+
   return (
     <div>
-      <Query query={NEWS_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Fetching products.....</div>
-          if (error) return <div>Error fetching products</div>
+      <Switch>
+        <Route path={`${match.path}/:articleId`}>
+          <ArticlePage />
+        </Route>
+        <Route path={match.path}>
+          <Query query={NEWS_QUERY}>
+            {({ loading, error, data }) => {
+              if (loading) return <div>Fetching products.....</div>
+              if (error) return <div>Error fetching products</div>
 
-          console.log(data);
-          const articles = data.allRecentNews;
-          return (
-            <main>
-              {articles.map(article => <ArticleListItem key={article.id} article={article} />)}
-            </main>
-          )
-        }}
-      </Query>
+              console.log(data);
+              const articles = data.allRecentNews;
+
+              return (
+                <main>
+                  <ul>
+                    {articles.map(article => <li>
+                      <Link to={`${match.url}/${article.id}`}>
+                        <ArticleListItem key={article.id} article={article} />
+                      </Link>
+                    </li>)}
+                  </ul>
+                </main>
+              )
+            }}
+          </Query>
+        </Route>
+      </Switch>
     </div>
   );
 
@@ -27,6 +51,10 @@ const RecentNews = () => {
 
 export default RecentNews;
 
+const ArticlePage = () => {
+  let { articleId } = useParams();
+  return <h3>Requested article ID: {articleId}</h3>;
+}
 
 const ArticleListItem = ({ article }) =>
   <Card style={{ width: "18rem" }}>
