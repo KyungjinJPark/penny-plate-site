50061e2fd091e5581f87ecef076aec015d5b9442
Explosivespectra <playonly88@gmail.com>
2021-01-01 17:40:54 -0500

    Added additional filters

--- src/components/all-products/queries.js
+++ src/components/all-products/queries.js
@@ -1,8 +1,8 @@
 import gql from "graphql-tag";
 
 const PRODUCTS_QUERY = gql`
-query ProductQuery($application: [Application!], $productType: [ProductType!]){
-  allProducts(where: {application_in: $application, productType_in: $productType}) 
+query ProductQuery($application: [Application!], $productType: [ProductType!], $shape: [Shape!], $stock: [StockType!]){
+  allProducts(where: {application_in: $application, productType_in: $productType, shape_in: $shape, stockType_in: $stock}) 
   {
     id
     itemNo
@@ -30,6 +30,24 @@ query {
   }
 }
 `;
+const SHAPE_QUERY = gql`
+query {
+  __type(name: "Shape") {
+    enumValues {
+      name
+    }
+  }
+}
+`;
+const STOCK_QUERY = gql`
+query {
+  __type(name: "StockType") {
+    enumValues {
+      name
+    }
+  }
+}
+`;
 const NEWS_QUERY = gql`
 query {
   allRecentNews {
@@ -44,4 +62,4 @@ query {
 `;
 
 
-export { PRODUCTS_QUERY, FILTER_QUERY, NEWS_QUERY, PTYPE_QUERY };
+export { PRODUCTS_QUERY, FILTER_QUERY, NEWS_QUERY, PTYPE_QUERY, SHAPE_QUERY, STOCK_QUERY };
