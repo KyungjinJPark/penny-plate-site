ed7a8a5abd5ed2a50b65874539b955c0ccfc2b61
Explosivespectra <playonly88@gmail.com>
2021-01-06 06:41:47 -0500

    Added ID detection for search feature

--- src/components/Products.js
+++ src/components/Products.js
@@ -91,7 +91,8 @@ const ProductsList = ({ currentFilter, currentPType, currentShape, currentStock,
   if (productError) return <div>Error fetching products</div>
   const items = productData.allProducts.filter((product => {
     const lowerProduct = product.description.toLowerCase();
-    return !(currentSearch.every((keyword) => {return lowerProduct.indexOf(keyword.toLowerCase()) == -1}));}));
+    const id = product.id.toLowerCase();
+    return !(currentSearch.every((keyword) => {return (lowerProduct.indexOf(keyword.toLowerCase()) == -1 && id.localeCompare(keyword.toLowerCase()) != 0)}));}));
   return (
     <div>
       <button onClick={showModal}>PDF Items ({cartitems.length})</button>
