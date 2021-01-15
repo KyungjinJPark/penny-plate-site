643eda76519c187bbfe87c347c71de6a05c5d9f0
KyungjinJPark <kjparkcontact@gmail.com>
2021-01-15 17:38:59 -0500

    added stopgag catalog and contact pages

---
+++ src/components/contactpage/ContactPage.js
@@ -0,0 +1,15 @@
+import { Container, Button } from "react-bootstrap";
+
+import "./contactpage.css";
+
+const ContactPage = () => {
+  return <>
+    <Container className="normal-container">
+      <h1>Contact Us</h1>
+      <div className="separator"></div>
+      <p>For questions, quote requests, or custom pan design requests, please complete the form below:</p>
+    </Container>
+  </>
+}
+
+export default ContactPage;
