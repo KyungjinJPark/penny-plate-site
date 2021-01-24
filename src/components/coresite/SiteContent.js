838d09715fa88c772278e36631f1a697f539be34
KJosiahPark <KyungjinJPark@gmail.com>
2021-01-24 02:28:23 -0500

    some minor changes / rearrangements

--- src/components/coresite/SiteContent.js
+++ src/components/coresite/SiteContent.js
@@ -1,7 +1,4 @@
-import {
-  Switch,
-  Route
-} from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "../homepage/HomePage";
 import ProductsPage from "../productspage/ProductsPage";
@@ -11,24 +8,12 @@ import NewItemsPage from "../newitemspage/NewItemsPage";
 import ContactPage from "../contactpage/ContactPage";
 
 const SiteContent = () => <Switch>
-  <Route exact path="/">
-    <HomePage />
-  </Route>
-  <Route path="/products">
-    <ProductsPage />
-  </Route>
-  <Route path="/about">
-    <AboutPage />
-  </Route>
-  <Route path="/catalog">
-    <CatalogPage />
-  </Route>
-  <Route path="/new-items">
-    <NewItemsPage />
-  </Route>
-  <Route path="/contact">
-    <ContactPage />
-  </Route>
+  <Route exact path="/" component={HomePage} />
+  <Route path="/products" component={ProductsPage} />
+  <Route path="/about" component={AboutPage} />
+  <Route path="/catalog" component={CatalogPage} />
+  <Route path="/new-items" component={NewItemsPage} />
+  <Route path="/contact" component={ContactPage} />
 </Switch>
 
 
