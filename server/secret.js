ba4519f7aaa4723f0bc27b44a4125dec40ccbcf5
KyungjinJPark <kjparkcontact@gmail.com>
2021-01-24 19:41:02 -0500

    separation complete, needs node v14+ to run server

---
+++ server/secret.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Gmail Username and Password for NodeMailer
+ * 
+ */
+
+module.exports = {
+  NODEMAILER_USERNAME: "pennyplatecontactform",
+  NODEMAILER_PASSWORD: "Qhk7gTSZym23662"
+}
