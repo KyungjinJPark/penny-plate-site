b43f5d94b59e6f441ffb41ee6569640c388295ab
KyungjinJPark <kjparkcontact@gmail.com>
2021-01-29 05:36:15 -0500

    connected contact page to api

--- client/src/components/contactpage/ContactPage.js
+++ client/src/components/contactpage/ContactPage.js
@@ -15,7 +15,6 @@ const ContactPage = () => {
           Ad blockers may block email requests. If this happens please disable the ad blocker or email info@pennyplate.com manually.
         </p>
       </div>
-      <ApiTest />
     </Container>
   )
 }
@@ -26,7 +25,7 @@ export default ContactPage;
 const ContactForm = () => {
   const [status, setStatus] = useState("Submit");
 
-  const mailURL = "https://pennyplate-email-test.herokuapp.com/contact/send-mail";
+  const mailURL = "/api/contact/send-mail";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -96,58 +95,3 @@ const ContactForm = () => {
     </form>
   );
 };
-
-class ApiTest extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-
-    this.setState({ responseToPost: body });
-  };
-
-  render() {
-    return (
-      <div>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-      </div>
-    );
-  }
-}
